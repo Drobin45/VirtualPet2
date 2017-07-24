d2cd6a9bc1210cac9865fc364e33bacb15f42431
drobin45 <drobin45@kent.edu>
2017-07-24 13:31:12 -0400

    Initial commit

---
+++ VirtualPet2.0/Program.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VirtualPet2._0
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            VirtualPet myCat = new VirtualPet("Danger", 0, 0, 0, 0, false, 0, 100);
+
+            int selectedOption;
+
+            do
+            {
+                //Console.Clear();    
+                myCat.CheckForSickness();
+                myCat.CheckForDeath();
+
+                Console.WriteLine();
+                Console.WriteLine("Please select an option");
+                Console.WriteLine("1. Feed the pet");
+                Console.WriteLine("2. Water the pet");
+                Console.WriteLine("3. Take it to to use the bathroom");
+                Console.WriteLine("4. Search for medicine!");
+                Console.WriteLine("5. Play");
+                Console.WriteLine("6. Go on an Adventure!");
+                Console.WriteLine("7. Quit");
+
+                selectedOption = int.Parse(Console.ReadLine());
+
+                switch (selectedOption)
+                {
+                    case 1:
+
+                        myCat.HungerDecrease();
+                        myCat.ThirstIncrease();
+                        myCat.BoredomeIncrease();
+                        myCat.WasteIncrease();
+                        myCat.WasteIncrease();
+                        myCat.ContractSicknessRoll();
+
+                        Console.WriteLine("Thank you for feeding the pet");
+                        break;
+
+                    case 2:
+                        myCat.ThirstDecrease();
+                        myCat.HungerIncrease();
+                        myCat.BoredomeIncrease();
+                        myCat.WasteIncrease();
+                        myCat.ContractSicknessRoll();
+
+                        Console.WriteLine("You FINALLY watered your pet! Don't forget, this dumb, little beast depends on you for survival!");
+                        break;
+
+                    case 3:
+                        myCat.EmptyWaste();
+                        myCat.ThirstIncrease();
+                        myCat.HungerIncrease();
+                        myCat.BoredomeIncrease();
+                        myCat.ContractSicknessRoll();
+                        Console.WriteLine("Your animal has relieved herself! Don't forget, everybody poops!");
+                        break;
+
+                    case 4:
+                        myCat.SearchForMedicine();
+                        myCat.ThirstIncrease();
+                        myCat.HungerIncrease();
+                        myCat.WasteIncrease();
+                        myCat.BoredomeIncrease();
+                        break;
+
+                    case 5:
+                        myCat.Play();
+                        myCat.ThirstIncrease();
+                        myCat.HungerIncrease();
+                        myCat.WasteIncrease();
+                        myCat.ContractSicknessRoll();
+                        Console.WriteLine("You frolic in the meadow with your beloved animal companion: what joy!");
+                        break;
+
+                    case 6:
+                        Console.WriteLine("You have disembarked from your master! What great freedom lies before your adventerous littel paws! With the future lying in wait, where will you let your little, fury destiny take you?");
+                        Console.WriteLine("1.Flee to the forest with your beastial brethren!\n 2. There is a town nearby, a full human hive! Truly, not all humans can be as bad as your prior master! \n 3. There is a wizard's tower to the South, you could take that human, and make the mighty fellow bow to your even mightier will!");
+
+
+                        break;
+
+                    case 7:
+
+                        Console.WriteLine("Thank you for playing.");
+                        break;
+
+                    default:
+
+                        Console.WriteLine("Invalid option selected.");
+                        break;
+                }
+
+            } while (selectedOption != 10);
+        }
+    }
+}
---
+++ VirtualPet2.0/VirtualPet.cs
@@ -0,0 +1,211 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VirtualPet2._0
+{
+    class VirtualPet
+    {
+
+        //Fields
+        private string name;
+        private int hunger;
+        private int thirst;
+        private int waste;
+        private int boredom;
+        private bool sickness;
+        private int tiredness;
+        private int hp;
+
+
+        //Constructors
+        public VirtualPet()
+        {
+            //Default
+        }
+
+        public VirtualPet(string name, int hunger, int sick, int boredem)
+        {
+            this.name = name;
+        }
+
+        public VirtualPet(string name, int hunger, int thirst, int waste, int boredom, bool sickness, int tiredness, int hp)
+        {
+            this.name = name;
+            this.hunger = hunger;
+            this.waste = waste;
+            this.thirst = thirst;
+            this.sickness = sickness;
+            this.boredom = boredom;
+            this.tiredness = tiredness;
+            this.hp = hp;
+
+        }
+
+        //Methods
+
+        public void HungerIncrease()
+        {
+
+            this.hunger = hunger + 3;
+        }
+
+
+        public void HungerDecrease()
+        {
+
+            this.hunger = hunger - 5;
+        }
+        //-------------------------------------------
+        public void ThirstIncrease()
+        {
+            this.thirst++;
+        }
+
+        public void ThirstDecrease()
+        {
+            this.thirst = thirst - 7;
+        }
+        //-------------------------------------------
+        public void WasteIncrease()
+        {
+            this.waste++;
+        }
+        public void EmptyWaste()
+        {
+            this.waste = 0;
+        }
+        //-------------------------------------------
+        public void Sickness()
+        {
+            this.sickness = true;
+            Console.WriteLine("Oh no, your pet has contracted a rare illness!");
+        }
+
+        public void Cured()
+        {
+            this.sickness = false;
+            Console.WriteLine("Your pet is cured!");
+        }
+        //-------------------------------------------
+        public void BoredomeIncrease()
+        {
+            this.boredom++;
+        }
+        public void Play()
+        {
+            this.boredom = 0;
+        }
+        //-------------------------------------------
+        public void TirednessIncrease()
+        {
+            this.tiredness = tiredness + 5;
+        }
+        //-------------------------------------------
+        public void ContractSicknessRoll()
+        {
+            Random sickRoll = new Random();
+            int sick = sickRoll.Next(0, 11);
+
+            if (sick == 1)
+            {
+                Console.WriteLine("Your poor little pet has fallen ill!");
+                this.sickness = true;
+            }
+        }
+
+        public void SearchForMedicine()
+        {
+            Random medicineRoll = new Random();
+            int medicine = medicineRoll.Next(0, 11);
+
+            if (medicine <= 6)
+            {
+                Console.WriteLine("Your poor little pet is finally cured!");
+                this.sickness = false;
+            }
+            else
+            {
+                Console.WriteLine("You seach, but have no luck");
+            }
+        }
+
+        public void CheckForSickness()
+        {
+            if (sickness == true)
+            {
+                SicknessDamage();
+            }
+            else
+            {
+                Console.WriteLine("Your pet is still healthy!");
+            }
+        }
+
+        public void SicknessDamage()
+        {
+            this.hp = hp - 10;
+            Console.WriteLine("Your pet is whithering with sickness! it loses 10 hp!");
+        }
+
+        //-------------------------------------------
+        public void CheckForDeath()
+        {
+
+            if (this.hunger == 15)
+            {
+                Console.WriteLine("Your cute animal has perished at your cruel, whithering hands...");
+                Console.WriteLine("Any last words?");
+                string words = Console.ReadLine();
+                Environment.Exit(0);
+            }
+            else if (this.thirst == 15)
+            {
+                Console.WriteLine("Your cute animal has perished at your cruel, whithering hands...");
+                Console.WriteLine("Any last words?");
+                string words = Console.ReadLine();
+                Environment.Exit(0);
+            }
+            else if (this.hp == 0)
+            {
+                Console.WriteLine("Your cute animal has perished at your cruel, whithering hands...");
+                Console.WriteLine("Any last words?");
+                string words = Console.ReadLine();
+                Environment.Exit(0);
+            }
+            else
+            {
+                MyPetStatus();
+            }
+        }
+        //-------------------------------------------
+        public void CheckWaste()
+        {
+            if (waste == 15)
+            {
+                Console.WriteLine("Your pet has violently soiled itself, and you belongings... It loses 25 hp");
+                this.hp = hp - 25;
+            }
+        }
+
+
+        //-------------------------------------------
+
+
+
+        public void MyPetStatus()
+        {
+            CheckWaste();
+            Console.WriteLine(name);
+            Console.WriteLine("Hunger: " + hunger);
+            Console.WriteLine("Thirst: " + thirst);
+            Console.WriteLine("Waste: " + waste);
+            Console.WriteLine("Boredom: " + boredom);
+            Console.WriteLine("Tiredness: " + tiredness);
+            Console.WriteLine("Your pet's health is " + hp + "/100");
+            //-------------------------------------------     
+        }
+        }
+    }
